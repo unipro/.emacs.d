fe9adefd8e055306cecb35f2fcb3c1d25e132cc2
Daniel Jun <unipro.kr@gmail.com>
2016-07-21 19:02:01 +0900

    First commit

---
+++ init.ec
@@ -0,0 +1,26 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (sanityinc-solarized-light)))
+ '(custom-safe-themes
+   (quote
+    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+ '(default-input-method "korean-hangul")
+ '(dired-omit-files "^\\.?#\\|^\\.$\\|^\\.\\.$\\|^\\..+$")
+ '(gc-cons-threshold 134217728)
+ '(global-undo-tree-mode t)
+ '(ido-enable-flex-matching t)
+ '(ido-everywhere t)
+ '(ido-mode (quote both) nil (ido))
+ '(safe-local-variable-values (quote ((no-byte-compile t))))
+ '(shift-select-mode nil)
+ '(windmove-wrap-around t)
+ '(winner-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ init.el
@@ -0,0 +1,164 @@
+;;; init.el -- My personal emacs settings
+
+;;; Commentary:
+
+;; This file bootstraps the configuration, which is divided into
+;; a number of other files.
+
+;;; Code:
+
+;;----------------------------------------------------------------------------
+;; Bootstrap config
+;;----------------------------------------------------------------------------
+(setq custom-file (expand-file-name "init.ec" user-emacs-directory))
+
+;;----------------------------------------------------------------------------
+;; Package manager settings
+;;----------------------------------------------------------------------------
+(require 'package)
+
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;; On-demand installation of packages
+
+(defun require-package (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (if (boundp 'package-selected-packages)
+            ;; Record this as a package the user installed explicitly
+            (package-install package nil)
+          (package-install package))
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
+
+
+(defun maybe-require-package (package &optional min-version no-refresh)
+  "Try to install PACKAGE, and return non-nil if successful.
+In the event of failure, return nil and print a warning message.
+Optionally require MIN-VERSION.  If NO-REFRESH is non-nil, the
+available package lists will not be re-downloaded in order to
+locate PACKAGE."
+  (condition-case err
+      (require-package package min-version no-refresh)
+    (error
+     (message "Couldn't install package `%s': %S" package err)
+     nil)))
+
+
+;;----------------------------------------------------------------------------
+;; Load common packages
+;;----------------------------------------------------------------------------
+(require-package 'diminish)
+
+
+;;----------------------------------------------------------------------------
+;; dired
+;;----------------------------------------------------------------------------
+(require 'dired-x)
+(setq dired-omit-mode t)
+(setq-default dired-omit-files-p t)
+
+
+;;----------------------------------------------------------------------------
+;; flycheck
+;;----------------------------------------------------------------------------
+(when (maybe-require-package 'flycheck)
+  (add-hook 'after-init-hook 'global-flycheck-mode)
+
+  ;; Override default flycheck triggers
+  (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
+	flycheck-idle-change-delay 0.8)
+
+  (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list))
+
+
+;;----------------------------------------------------------------------------
+;; undo-tree
+;;----------------------------------------------------------------------------
+(require-package 'undo-tree)
+(diminish 'undo-tree-mode)
+
+
+;;----------------------------------------------------------------------------
+;; Editing
+;;----------------------------------------------------------------------------
+(require-package 'whole-line-or-region)
+
+(global-set-key [remap dabbrev-expand] 'hippie-expand)
+
+ 
+;;----------------------------------------------------------------------------
+;; Locales
+;;----------------------------------------------------------------------------
+(when window-system
+  (setq coding-system-for-read 'utf-8))
+
+
+;;----------------------------------------------------------------------------
+;; windmove
+;;----------------------------------------------------------------------------
+(windmove-default-keybindings)
+
+
+;;----------------------------------------------------------------------------
+;; helm
+;;----------------------------------------------------------------------------
+(require-package 'helm)
+(require 'helm-config)
+
+
+;;----------------------------------------------------------------------------
+;; Fonts
+;;----------------------------------------------------------------------------
+(when (display-graphic-p)
+  (let ((fontset "fontset-default"))
+    (cond ((member "Droid Sans Mono" (font-family-list))
+           (set-frame-font "Droid Sans Mono")
+           (set-face-font 'default "Droid Sans Mono"))
+          ((member "DejaVu Sans Mono" (font-family-list))
+           (set-frame-font "DejaVu Sans Mono")
+           (set-face-font 'default "DejaVu Sans Mono")))
+    (when (member "나눔고딕코딩" (font-family-list))
+      (set-fontset-font fontset 'hangul
+                        '("NanumGothicCoding" . "unicode-bmp")))))
+
+
+;;----------------------------------------------------------------------------
+;; Themes
+;;----------------------------------------------------------------------------
+(require-package 'color-theme-sanityinc-solarized)
+
+
+;;----------------------------------------------------------------------------
+;; misc
+;;----------------------------------------------------------------------------
+(global-set-key (kbd "M-i") 'imenu)
+
+
+;;----------------------------------------------------------------------------
+;; Allow access from emacsclient
+;;----------------------------------------------------------------------------
+(require 'server)
+(unless (server-running-p)
+  (server-start))
+
+
+;;----------------------------------------------------------------------------
+;; Variables configured via the interactive 'customize' interface
+;;----------------------------------------------------------------------------
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+(provide 'init)
+;;; init.el ends here
