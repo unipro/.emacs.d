b46dd77bea35eef3f287c7c846bd8752efa7a7cd
Daniel Jun <unipro.kr@gmail.com>
2016-11-19 01:14:35 +0900

    Update custom.el

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(custom-enabled-themes (quote (sanityinc-solarized-light)))
  '(package-selected-packages
    (quote
-    (js2-mode json-mode exec-path-from-shell package-install-file whole-line-or-region whitespace-cleanup-mode use-package undo-tree sr-speedbar slime paredit-everywhere page-break-lines nginx-mode markdown-mode magit lua-mode helm-projectile helm-ls-git helm-gtags helm-gitignore helm-descbinds helm-company ggtags geiser flycheck-clojure elpy ein dropdown-list company-statistics company-c-headers color-theme-sanityinc-solarized cmake-mode c-eldoc browse-kill-ring))))
+    (jdee js2-mode json-mode exec-path-from-shell package-install-file whole-line-or-region whitespace-cleanup-mode use-package undo-tree sr-speedbar slime paredit-everywhere page-break-lines nginx-mode markdown-mode magit lua-mode helm-projectile helm-ls-git helm-gtags helm-gitignore helm-descbinds helm-company ggtags geiser flycheck-clojure elpy ein dropdown-list company-statistics company-c-headers color-theme-sanityinc-solarized cmake-mode c-eldoc browse-kill-ring))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
