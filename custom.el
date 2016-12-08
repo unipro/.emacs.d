fdf099915d8952cb4ffe5b4ab24972fc29c1ee81
Byungwan Jun <byungwan.jun@inisoft.co.kr>
2016-12-09 00:51:52 +0900

    Update custom.el

--- custom.el
+++ custom.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (sanityinc-solarized-light)))
  '(package-selected-packages
    (quote
     (jdee js2-mode json-mode exec-path-from-shell package-install-file whole-line-or-region whitespace-cleanup-mode use-package undo-tree sr-speedbar slime paredit-everywhere page-break-lines nginx-mode markdown-mode magit lua-mode helm-projectile helm-ls-git helm-gtags helm-gitignore helm-descbinds helm-company ggtags geiser flycheck-clojure elpy ein dropdown-list company-statistics company-c-headers color-theme-sanityinc-solarized cmake-mode c-eldoc browse-kill-ring))))
