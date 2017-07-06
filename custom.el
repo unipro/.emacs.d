dfe876db355be6b6dba5ff212a6eb3b0026fe3f7
unipro <unipro.kr@gmail.com>
2017-07-06 17:03:44 +0900

    Update custom.el

--- custom.el
+++ custom.el
@@ -3,9 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (sanityinc-solarized-light)))
+ '(custom-safe-themes
+   (quote
+    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
  '(package-selected-packages
    (quote
-    (jdee js2-mode json-mode exec-path-from-shell package-install-file whole-line-or-region whitespace-cleanup-mode use-package undo-tree sr-speedbar slime paredit-everywhere page-break-lines nginx-mode markdown-mode magit lua-mode helm-projectile helm-ls-git helm-gtags helm-gitignore helm-descbinds helm-company ggtags geiser flycheck-clojure elpy ein dropdown-list company-statistics company-c-headers color-theme-sanityinc-solarized cmake-mode c-eldoc browse-kill-ring))))
+    (ace-window web-mode opencl-mode elixir-mode jdee js2-mode json-mode exec-path-from-shell package-install-file whole-line-or-region whitespace-cleanup-mode use-package undo-tree sr-speedbar slime paredit-everywhere page-break-lines nginx-mode markdown-mode magit lua-mode helm-projectile helm-ls-git helm-gtags helm-gitignore helm-descbinds helm-company ggtags geiser flycheck-clojure elpy ein dropdown-list company-statistics company-c-headers color-theme-sanityinc-solarized cmake-mode c-eldoc browse-kill-ring))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
