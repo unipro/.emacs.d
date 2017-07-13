7543f5377ddf96af4673022384982bca34a1b792
Daniel Jun <unipro.kr@gmail.com>
2017-07-14 07:23:02 +0900

    Update custom.el

--- custom.el
+++ custom.el
@@ -3,9 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (sanityinc-solarized-light)))
  '(package-selected-packages
    (quote
-    (jdee js2-mode json-mode exec-path-from-shell package-install-file whole-line-or-region whitespace-cleanup-mode use-package undo-tree sr-speedbar slime paredit-everywhere page-break-lines nginx-mode markdown-mode magit lua-mode helm-projectile helm-ls-git helm-gtags helm-gitignore helm-descbinds helm-company ggtags geiser flycheck-clojure elpy ein dropdown-list company-statistics company-c-headers color-theme-sanityinc-solarized cmake-mode c-eldoc browse-kill-ring))))
+    (ace-window opencl-mode erlang elixir-mode yaml-mode whole-line-or-region whitespace-cleanup-mode use-package undo-tree sr-speedbar slime psvn paredit-everywhere page-break-lines nginx-mode markdown-mode magit lua-mode json-mode js2-mode jdee helm-projectile helm-ls-git helm-gtags helm-gitignore helm-descbinds helm-company ggtags geiser flycheck-clojure exec-path-from-shell elpy ein dropdown-list company-statistics company-c-headers color-theme-sanityinc-solarized cmake-mode c-eldoc browse-kill-ring))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
