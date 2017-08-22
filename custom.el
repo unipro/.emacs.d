fedd032b1ac93e911823d6249c38fef67dc10fca
unipro <unipro.kr@gmail.com>
2017-08-22 10:06:11 +0900

    Update custom.el

--- custom.el
+++ custom.el
@@ -9,7 +9,7 @@
     ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
  '(package-selected-packages
    (quote
-    (ace-window web-mode opencl-mode elixir-mode jdee js2-mode json-mode exec-path-from-shell package-install-file whole-line-or-region whitespace-cleanup-mode use-package undo-tree sr-speedbar slime paredit-everywhere page-break-lines nginx-mode markdown-mode magit lua-mode helm-projectile helm-ls-git helm-gtags helm-gitignore helm-descbinds helm-company ggtags geiser flycheck-clojure elpy ein dropdown-list company-statistics company-c-headers color-theme-sanityinc-solarized cmake-mode c-eldoc browse-kill-ring))))
+    (yasnippet yaml-mode whole-line-or-region whitespace-cleanup-mode web-mode vala-mode use-package undo-tree slime psvn paredit-everywhere page-break-lines opencl-mode nginx-mode markdown-mode magit lua-mode json-mode js2-mode helm-projectile helm-gtags helm-descbinds helm-company ggtags geiser flycheck erlang elixir-mode company-statistics company-c-headers color-theme-sanityinc-solarized cider c-eldoc browse-kill-ring ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
