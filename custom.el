f992d52a4519753214b8393665b44344d5998cd1
unipro <unipro.kr@gmail.com>
2017-08-22 12:32:54 +0900

    Update custom.el

--- custom.el
+++ custom.el
@@ -3,13 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (sanityinc-solarized-light)))
- '(custom-safe-themes
-   (quote
-    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
- '(package-selected-packages
-   (quote
-    (yasnippet yaml-mode whole-line-or-region whitespace-cleanup-mode web-mode vala-mode use-package undo-tree slime psvn paredit-everywhere page-break-lines opencl-mode nginx-mode markdown-mode magit lua-mode json-mode js2-mode helm-projectile helm-gtags helm-descbinds helm-company ggtags geiser flycheck erlang elixir-mode company-statistics company-c-headers color-theme-sanityinc-solarized cider c-eldoc browse-kill-ring ace-window))))
+ '(custom-enabled-themes (quote (sanityinc-solarized-light))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
