01fa3536818293b1a35e8c962bcf059c2480d651
unipro <unipro.kr@gmail.com>
2017-08-22 12:30:26 +0900

    Add scala mode

--- init.el
+++ init.el
@@ -683,6 +683,26 @@
 
 
 ;;------------------------------------------------------------------------------
+;; Scala
+;;------------------------------------------------------------------------------
+(use-package ensime
+  :ensure t)
+
+(use-package scala-mode
+  :interpreter
+  ("scala" . scala-mode))
+
+(use-package sbt-mode
+  :commands sbt-start sbt-command
+  :config
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
+  (substitute-key-definition 'minibuffer-complete-word
+                             'self-insert-command
+                             minibuffer-local-completion-map))
+
+
+;;------------------------------------------------------------------------------
 ;; scheme & geiser
 ;;------------------------------------------------------------------------------
 (add-hook 'scheme-mode-hook (lambda ()
