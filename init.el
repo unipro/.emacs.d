56b2d0e530630109d44d99167f240149ff7ed55c
unipro <unipro.kr@gmail.com>
2018-02-20 18:03:08 +0900

    electric-pair-mode and python triple quotes

--- init.el
+++ init.el
@@ -778,6 +778,21 @@
   (setq-default indent-tabs-mode nil)
   (add-hook 'python-mode-hook #'highlight-indent-guides-mode)
 
+  ;; electric-pair-mode and python triple quotes
+  (defun python-electric-pair-string-delimiter ()
+    (when (and electric-pair-mode
+               (memq last-command-event '(?\" ?\'))
+               (let ((count 0))
+                 (while (eq (char-before (- (point) count)) last-command-event)
+                   (setq count (1+ count)))
+                 (= count 3)))
+      (save-excursion (insert (make-string 3 last-command-event)))))
+
+  (add-hook 'python-mode-hook
+            (lambda ()
+              (add-hook 'post-self-insert-hook
+                        #'python-electric-pair-string-delimiter 'append t)))
+
   :config
   (setq python-indent-offset 4))
 
