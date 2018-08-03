aaffa826d7ae7d30eb6daa8769b5309c2cd822dd
Ian Jun <ian@mykoon.com>
2018-08-03 14:47:37 +0900

    Update

--- init.el
+++ init.el
@@ -196,8 +196,8 @@
 ;; Windows, Frames, Fonts, Themes
 ;;----------------------------------------------------------------------------
 (when (display-graphic-p)
-  (menu-bar-mode -1)
-  ;; (tool-bar-mode -1)
+  ;; (menu-bar-mode -1)
+  (tool-bar-mode -1)
   ;; (scroll-bar-mode -1)
   (set-scroll-bar-mode 'right)
   (setq use-dialog-box nil)
